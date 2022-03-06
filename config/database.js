c3bb2d4446f5cf3325b848d192c977933f6085ea
Phat289 <97430120+Phat289@users.noreply.github.com>
2022-03-07 00:41:31 +0700

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'url' : 'mongodb+srv://sonnt:MSSVpd02723@cluster0.4nteg.mongodb.net/BanHang?retryWrites=true&w=majority'
+
+    //mongodb+srv://<username>:<password>@cluster0.4tew3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+};
